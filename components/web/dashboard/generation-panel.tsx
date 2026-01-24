8a305b2b43399403dc226980f25d81fd0993a7b0
Arjun <arjun.ambavane@gmail.com>
2026-01-24 23:13:00 +0530

    feat(ui): add controls to canvas, proper loading states and prevent layout shift with canvas skeleton

--- components/web/dashboard/generation-panel.tsx
+++ components/web/dashboard/generation-panel.tsx
@@ -18,18 +18,24 @@ import {
     FieldLabel,
 } from "@/components/ui/field"
 import { Button } from '@/components/ui/button';
-import { Eraser, Send } from 'lucide-react';
+import { Eraser, Pen, Redo, Send, Trash2, Undo } from 'lucide-react';
 import { Textarea } from '@/components/ui/textarea';
 import { useTheme } from 'next-themes';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import { toast } from 'sonner';
 import { useMutation } from 'convex/react';
+import { Spinner } from '@/components/ui/spinner';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function GenerationPanel() {
 
     const { resolvedTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
+    const [isErasing, setIsErasing] = useState(false);
+    const [isGenerating, setIsGenerating] = useState(false);
+
+    const canvasRef = useRef<ReactSketchCanvasRef>(null);
 
     useEffect(() => {
         setMounted(true);
@@ -40,6 +46,8 @@ export default function GenerationPanel() {
 
     const form = useForm<z.infer<typeof promptSchema>>({
         resolver: zodResolver(promptSchema),
+        mode: "onSubmit",
+        reValidateMode: "onSubmit",
         defaultValues: {
             prompt: ""
         },
@@ -47,6 +55,7 @@ export default function GenerationPanel() {
 
     const onSubmit = async (formData: z.infer<typeof promptSchema>) => {
         if (!canvasRef.current) return;
+        setIsGenerating(true);
         try {
             const canvasDataUrl = await canvasRef.current.exportImage('jpeg');
             const imageUploadUrl = await generateUploadUrl();
@@ -65,31 +74,80 @@ export default function GenerationPanel() {
                 prompt: formData.prompt,
                 canvasImageStorageId: storageId,
             });
-
+            toast.success("Image generation started!");
         } catch (err: unknown) {
             const errorMessage = err instanceof Error ? err.message : "Some unexpected error occured. Please try again"
             toast.error(errorMessage);
+        } finally {
+            setIsGenerating(false);
         }
     }
 
-    const canvasRef = useRef<ReactSketchCanvasRef>(null);
+    const handleUndo = () => canvasRef.current?.undo();
+
+    const handleRedo = () => canvasRef.current?.redo();
+
+    const handleClearCanvas = () => canvasRef.current?.clearCanvas();
+
+    const toggleEraser = () => {
+        setIsErasing(!isErasing);
+        if (!isErasing) canvasRef.current?.eraseMode(true);
+        else canvasRef.current?.eraseMode(false);
+    };
+
     return (
         <div className="col-span-1 flex flex-col gap-8 justify-center items-center h-full">
-            {mounted && (
-                <ReactSketchCanvas
-                    ref={canvasRef}
-                    style={{
-                        border: '0.0625rem solid #9c9c9c',
-                        borderRadius: '0.25rem',
-                        overflow: 'hidden',
-                    }}
-                    strokeWidth={4}
-                    strokeColor={resolvedTheme === 'dark' ? '#e8e8e8' : '#000000'}
-                    canvasColor={resolvedTheme === 'dark' ? '#121212' : '#f1f5f9'}
-                    className='w-full h-80!'
-                />
-            )}
-            <Card className="w-full">
+            <div className='w-full relative h-80'>
+                {!mounted ? (
+                    <Skeleton className="size-full" />
+                ) : (
+                    <>
+                        <ReactSketchCanvas
+                            ref={canvasRef}
+                            strokeWidth={4}
+                            strokeColor={resolvedTheme === 'dark' ? '#e8e8e8' : '#000000'}
+                            canvasColor={resolvedTheme === 'dark' ? '#121212' : '#f1f5f9'}
+                            className='relative overflow-hidden border-border!'
+                        />
+                        <div className='flex items-center gap-2 absolute top-3 right-3'>
+                            <Button
+                                type="button"
+                                variant={isErasing ? "default" : "outline"}
+                                size="icon-sm"
+                                onClick={toggleEraser}
+                            >
+                                {isErasing ? <Pen /> : <Eraser />}
+                            </Button>
+                            <Button
+                                type="button"
+                                variant="outline"
+                                size="icon-sm"
+                                onClick={handleUndo}
+                            >
+                                <Undo />
+                            </Button>
+                            <Button
+                                type="button"
+                                variant="outline"
+                                size="icon-sm"
+                                onClick={handleRedo}
+                            >
+                                <Redo />
+                            </Button>
+                            <Button
+                                type="button"
+                                variant="destructive"
+                                size="icon-sm"
+                                onClick={handleClearCanvas}
+                            >
+                                <Trash2 />
+                            </Button>
+                        </div>
+                    </>
+                )}
+            </div>
+
+            <Card className="w-full border-l">
                 <CardContent>
                     <form id='prompt-form' onSubmit={form.handleSubmit(onSubmit)}>
                         <FieldGroup>
@@ -98,8 +156,8 @@ export default function GenerationPanel() {
                                 control={form.control}
                                 render={({ field, fieldState }) => (
                                     <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="prompt" className='text-sm'>
-                                            Prompt
+                                        <FieldLabel htmlFor="prompt" className='text-sm mb-1'>
+                                            Describe what you want to generate
                                         </FieldLabel>
                                         <Textarea
                                             {...field}
@@ -108,7 +166,13 @@ export default function GenerationPanel() {
                                             placeholder="Enter your prompt"
                                             autoComplete="off"
                                             rows={3}
-                                            className="resize-none h-18 overflow-y-auto"
+                                            className="resize-none h-20 overflow-y-auto"
+                                            onChange={(e) => {
+                                                field.onChange(e);
+                                                if (e.target.value.length >= 3) {
+                                                    form.clearErrors("prompt");
+                                                }
+                                            }}
                                         />
                                         {fieldState.invalid && (
                                             <FieldError errors={[fieldState.error]} />
@@ -120,16 +184,25 @@ export default function GenerationPanel() {
                     </form>
                 </CardContent>
                 <CardFooter>
-                    <Field orientation="horizontal">
-                        <Button size={"lg"} type="button" variant="outline" onClick={() => form.reset()}>
-                            <Eraser />
-                            Clear
-                        </Button>
-                        <Button size={"lg"} type="submit" form="prompt-form" className='flex-1'>
-                            <Send />
-                            Generate Image
-                        </Button>
-                    </Field>
+                    <Button
+                        size={"lg"}
+                        type="submit"
+                        form="prompt-form"
+                        disabled={isGenerating}
+                        className='flex-1'
+                    >
+                        {isGenerating ? (
+                            <>
+                                <Spinner />
+                                Generating...
+                            </>
+                        ) : (
+                            <>
+                                <Send />
+                                Generate
+                            </>
+                        )}
+                    </Button>
                 </CardFooter>
             </Card>
         </div>
